683d0b66a4ea6d7bbfeaa5d1273808962347402a
Ederson Franzen Fagundes <edersonff_@hotmail.com.br>
2023-05-07 22:58:07 -0300

    fix(build): logo vercel

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
           <div className="mb-6 md:mb-0">
             <a href="https://Merlin.com/" className="flex items-center">
               <img
-                src="/merlin/logo.png"
+                src="/Merlin/logo.png"
                 className="h-8 mr-3"
                 alt="Merlin Logo"
               />

5cc053da9198fc222087829211ab2141127dfcd7
Ederson Franzen Fagundes <edersonff_@hotmail.com.br>
2023-05-07 22:34:55 -0300

    fix: home feature content

--- src/Components/Home/Feature/index.tsx
+++ src/Components/Home/Feature/index.tsx
@@ -6,10 +6,10 @@ export default function HomeFeature() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-indigo-600">
-            Deploy faster
+            Facilidade de uso
           </h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Everything you need to deploy your app
+            Tudo o que você precisa para implantar seu aplicativo
           </p>
           <p className="mt-6 text-lg leading-8 text-gray-600">
             Quis tellus eget adipiscing convallis sit sit eget aliquet quis.

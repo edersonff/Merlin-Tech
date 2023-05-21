48ca072faa88e6e5c8a33a03b4d3712018982ad8
Ederson Franzen Fagundes <edersonff_@hotmail.com.br>
2023-05-21 01:57:01 -0300

    feat: search hover effect

--- src/Components/Home/Search/index.tsx
+++ src/Components/Home/Search/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function HomeSearch() {
   return (
     <div className="w-full center">
-      <div className="content flex items-center shadow-[5px_5px_0px_0px_#646464] border border-gray-500 rounded-full bg-white px-8 py-4 gap-x-7">
+      <div className="content relative top-0 left-0 flex items-center transition-all shadow-[5px_5px_0px_0px_#646464] border border-gray-500 rounded-full bg-white px-8 py-4 gap-x-7 search-home">
         <MagnifyingGlassIcon className="h-8 w-8" />
         <span className="text-xl lg:text-3xl font-light">Empreender é </span>
         <span className="text-xl lg:text-3xl font-bold">

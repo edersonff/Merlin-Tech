ab513e777a46c0398a98be31ccc37a81130ea540
Ederson Franzen Fagundes <edersonff_@hotmail.com.br>
2023-05-21 00:51:38 -0300

    fix: header contact items

--- src/Components/Home/Header/Contact/index.tsx
+++ src/Components/Home/Header/Contact/index.tsx
@@ -8,24 +8,26 @@ import {
   XIcon,
   XCircleIcon,
   XCircleIcon as XCircleIconSolid,
+  MapPinIcon,
+  ChatBubbleOvalLeftIcon,
 } from "@heroicons/react/20/solid";
 
 export default function HeaderContact() {
   return (
-    <div className="w-full p-2 bg-white border-b border-gray-200">
+    <div className="w-full p-2 center text-white">
       <div
-        className="flex items-center justify-center gap-4"
+        className="flex items-center justify-end gap-4 content"
         style={{
-          fontSize: "0.75rem",
+          fontSize: "0.65rem",
         }}
       >
         <div className="flex items-center justify-center">
-          <PhoneIcon className="h-5 w-5 text-gray-400" />
-          <span className="ml-2 text-gray-400">+55 11 99999-9999</span>
+          <ChatBubbleOvalLeftIcon className="h-4 w-4" />
+          <span className="ml-2">+55 11 99999-9999</span>
         </div>
         <div className="flex items-center justify-center ml-4">
-          <RocketLaunchIcon className="h-5 w-5 text-gray-400" />
-          <span className="ml-2 text-gray-400">@merliny</span>
+          <MapPinIcon className="h-4 w-4" />
+          <span className="ml-2">Willy Dorow 29, Rau, Jaraguá do Sul</span>
         </div>
       </div>
     </div>

4dfac6c817ef18590358482085040f1084d406aa
Ederson Franzen Fagundes <edersonff_@hotmail.com.br>
2023-05-25 00:09:52 -0300

    fix: text-align TextCard

--- src/Components/Geral/TextCard/index.tsx
+++ src/Components/Geral/TextCard/index.tsx
@@ -9,10 +9,11 @@ export default function TextCard({
 }: TextCardProps) {
   const marginLeft = pl ? "lg:pl-6" : "";
   const items = align === "left" ? "items-start" : "items-end";
+  const alignText = align === "left" ? "text-left" : "text-right";
 
   return (
     <div
-      className={`flex flex-1 flex-col gap-y-4 p-2 ${marginLeft} text-${align} ${items}`}
+      className={`flex flex-1 flex-col gap-y-4 p-2 ${marginLeft} ${alignText} ${items}`}
     >
       <h3 className={`text-5xl lg:w-3/4 font-semibold text-purple-900`}>
         {title}

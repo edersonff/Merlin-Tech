751f27bc53c0a21cc9d147802d36aaca9778c65c
Ederson Franzen Fagundes <edersonff_@hotmail.com.br>
2023-07-08 17:03:30 -0300

    fix(projects): repeat

--- src/Components/Home/Projects/data.ts
+++ src/Components/Home/Projects/data.ts
@@ -21,4 +21,7 @@ export const projectsData = [
   "/Images/Projects/mockup-5.webp",
   "/Images/Projects/mockup-1.webp",
   "/Images/Projects/mockup-2.webp",
+  "/Images/Projects/mockup-3.webp",
+  "/Images/Projects/mockup-4.webp",
+  "/Images/Projects/mockup-5.webp",
 ];

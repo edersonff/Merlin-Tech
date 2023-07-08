893325cd042ee30aa44737e369188a2a2be043ca
Ederson Franzen Fagundes <edersonff_@hotmail.com.br>
2023-07-08 17:07:32 -0300

    feat(vercel): analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,11 +6,13 @@ import "swiper/css/navigation";
 
 import Layout from "@/Components/Layout";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }

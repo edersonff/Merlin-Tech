a7d0b670819ac8f1b88d748baf8c34df06af10d8
Ederson Franzen Fagundes <edersonff_@hotmail.com.br>
2023-07-19 18:31:46 -0300

    fix: google analytics & clarity

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,10 +7,37 @@ import "swiper/css/navigation";
 import Layout from "@/Components/Layout";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-TVXHS1F4S7"
+      />
+      <Script
+        id="google-analytics"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-TVXHS1F4S7');`,
+        }}
+      />
+      <Script
+        id="clarity_script"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "i1h18978rv");`,
+        }}
+      />
       <Component {...pageProps} />
       <Analytics />
     </Layout>
